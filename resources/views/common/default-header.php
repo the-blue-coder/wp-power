a561952f7042dc068b573f1037dea87e89a2a49c
Jean R <jd.rakotoarison@gmail.com>
2021-01-24 21:05:52 +0300

    add random version number to styles and scripts

--- resources/views/common/default-header.php
+++ resources/views/common/default-header.php
@@ -5,8 +5,8 @@
         <meta name="viewport" content="width=device-width, initial-scale=1" />
 
         <link href="<?php echo WP_POWER_DIST_URL . '/bootstrap.min.css' ?>" rel="stylesheet" />
-        <link href="<?php echo WP_POWER_DIST_URL . '/custom.min.css' ?>" rel="stylesheet" />
-        <link href="<?php echo WP_POWER_DIST_URL . '/admin.min.css' ?>" rel="stylesheet" />
+        <link href="<?php echo WP_POWER_DIST_URL . '/custom.min.css?v=' . rand(1, 100000); ?>" rel="stylesheet" />
+        <link href="<?php echo WP_POWER_DIST_URL . '/admin.min.css?v=' . rand(1, 100000); ?>" rel="stylesheet" />
 
         <title><?php echo get_the_title(); ?></title>
     </head>
