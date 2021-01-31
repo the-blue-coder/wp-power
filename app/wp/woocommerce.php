7aa6ff8f0c7e101413d956b24ad23d873cddfe52
Jean R <jd.rakotoarison@gmail.com>
2021-01-31 20:18:19 +0300

    update woocommerce paths

--- app/wp/woocommerce.php
+++ app/wp/woocommerce.php
@@ -6,3 +6,18 @@
 add_action('after_setup_theme', function () {
     add_theme_support('woocommerce');
 });
+
+/**
+ * Templates overriding
+ */
+add_filter('woocommerce_locate_template', function ($template, $templateName, $templatePath) {
+    $re = '/woocommerce\/(templates\/)?(.*)/m';
+
+    preg_match($re, $template, $matches);
+
+    if (isset($matches[2]) && !empty($matches[2]) && file_exists( WP_POWER_VIEWS_DIR . '/woocommerce/' . $matches[2] )) {
+        $template = WP_POWER_VIEWS_DIR . '/woocommerce/' . $matches[2];
+    }
+
+    return $template;
+}, 10, 3);
--- config/paths.php
+++ config/paths.php
@@ -2,4 +2,4 @@
 
 define('WP_POWER_DIST_URL', get_stylesheet_directory_uri() . '/dist');
 define('WP_POWER_IMAGES_URL', WP_POWER_DIST_URL . '/images');
-define('WP_POWER_VIEWS_DIR', get_stylesheet_directory());
+define('WP_POWER_VIEWS_DIR', get_stylesheet_directory() . '/resources/views');
