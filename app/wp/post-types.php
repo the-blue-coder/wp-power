1e630e66620792f099179c3fa7136b299fe72187
Jean R <jd.rakotoarison@gmail.com>
2021-02-09 10:10:24 +0300

    post type loading code reorganized

--- app/wp/post-types.php
+++ app/wp/post-types.php
@@ -4,22 +4,30 @@
  * Post type name
  */
 // add_action('init', function () {
+//     $labels = [
+//         'name' => __('Plural', WP_POWER_TEXT_DOMAIN),
+//         'singular_name' => __('Singular', WP_POWER_TEXT_DOMAIN),
+//         'add_new' => __('Add new', WP_POWER_TEXT_DOMAIN),
+//         'add_new_item' => __('Add new singular', WP_POWER_TEXT_DOMAIN),
+//         'edit' => __('Edit', WP_POWER_TEXT_DOMAIN),
+//         'edit_item' => __('Edit singular', WP_POWER_TEXT_DOMAIN),
+//         'new_item' => __('New singular', WP_POWER_TEXT_DOMAIN),
+//         'view' => __('View singular', WP_POWER_TEXT_DOMAIN),
+//         'view_item' => __('View singular', WP_POWER_TEXT_DOMAIN),
+//         'search_items' => __('Search plural', WP_POWER_TEXT_DOMAIN),
+//         'not_found' => __('No plural found', WP_POWER_TEXT_DOMAIN),
+//         'not_found_in_trash' => __('No plural found in trash', WP_POWER_TEXT_DOMAIN),
+//         // 'parent' => __('Parent singular', WP_POWER_TEXT_DOMAIN)
+//     ];
+
+//     $supports = [
+//         'title',
+//         'editor',
+//         'custom-fields'
+//     ];
+
 //     register_post_type('slug', [
-//         'labels' => [
-//             'name' => __('Plural', WP_POWER_TEXT_DOMAIN),
-//             'singular_name' => __('Singular', WP_POWER_TEXT_DOMAIN),
-//             'add_new' => __('Add new', WP_POWER_TEXT_DOMAIN),
-//             'add_new_item' => __('Add new singular', WP_POWER_TEXT_DOMAIN),
-//             'edit' => __('Edit', WP_POWER_TEXT_DOMAIN),
-//             'edit_item' => __('Edit singular', WP_POWER_TEXT_DOMAIN),
-//             'new_item' => __('New singular', WP_POWER_TEXT_DOMAIN),
-//             'view' => __('View singular', WP_POWER_TEXT_DOMAIN),
-//             'view_item' => __('View singular', WP_POWER_TEXT_DOMAIN),
-//             'search_items' => __('Search plural', WP_POWER_TEXT_DOMAIN),
-//             'not_found' => __('No plural found', WP_POWER_TEXT_DOMAIN),
-//             'not_found_in_trash' => __('No plural found in trash', WP_POWER_TEXT_DOMAIN),
-//             // 'parent' => __('Parent singular', WP_POWER_TEXT_DOMAIN)
-//         ],
+//         'labels' => $labels,
 //         'public' => true,
 //         'publicly_queryable' => true,
 //         'show_ui' => true,
@@ -29,10 +37,6 @@
 //         'show_in_menu' => true,
 //         'exclude_from_search' => false,
 //         'rewrite' => true,
-//         'supports' => [
-//             'title',
-//             'editor',
-//             'custom-fields'
-//         ]
+//         'supports' => $supports
 //     ]);
 // });
