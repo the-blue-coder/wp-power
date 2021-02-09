81f5c92e45650ef2244ca30b190f56893d376b62
Jean R <jd.rakotoarison@gmail.com>
2021-02-09 09:52:56 +0300

    update post type registration script

--- app/wp/post-types.php
+++ app/wp/post-types.php
@@ -6,8 +6,19 @@
 // add_action('init', function () {
 //     register_post_type('slug', [
 //         'labels' => [
-//             'name' => 'Plural',
-//             'singular_name' => 'Singular'
+//             'name' => __('Plural', WP_POWER_TEXT_DOMAIN),
+//             'singular_name' => __('Singular', WP_POWER_TEXT_DOMAIN),
+//             'add_new' => __('Add new', WP_POWER_TEXT_DOMAIN),
+//             'add_new_item' => __('Add new singular', WP_POWER_TEXT_DOMAIN),
+//             'edit' => __('Edit', WP_POWER_TEXT_DOMAIN),
+//             'edit_item' => __('Edit singular', WP_POWER_TEXT_DOMAIN),
+//             'new_item' => __('New singular', WP_POWER_TEXT_DOMAIN),
+//             'view' => __('View singular', WP_POWER_TEXT_DOMAIN),
+//             'view_item' => __('View singular', WP_POWER_TEXT_DOMAIN),
+//             'search_items' => __('Search plural', WP_POWER_TEXT_DOMAIN),
+//             'not_found' => __('No plural found', WP_POWER_TEXT_DOMAIN),
+//             'not_found_in_trash' => __('No plural found in trash', WP_POWER_TEXT_DOMAIN),
+//             // 'parent' => __('Parent singular', WP_POWER_TEXT_DOMAIN)
 //         ],
 //         'public' => true,
 //         'publicly_queryable' => true,
--- config/global.php
+++ config/global.php
@@ -2,4 +2,5 @@
 
 define('WP_POWER_APP_NAME', 'WP Power');
 define('WP_POWER_APP_SLUG', 'wp-power');
+define('WP_POWER_TEXT_DOMAIN', 'wp-power');
 define('WP_POWER_ADMIN_EMAIL', 'email@example.com');
