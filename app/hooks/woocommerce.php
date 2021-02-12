28be61488a4f9556f181ab985f49f1795028b748
Jean R <jd.rakotoarison@gmail.com>
2021-02-12 11:56:03 +0300

    update woocommerce hooks

--- app/hooks/woocommerce.php
+++ app/hooks/woocommerce.php
@@ -26,12 +26,14 @@ add_action('after_setup_theme', function () {
  * Templates overriding
  */
 add_filter('woocommerce_locate_template', function ($template, $templateName, $templatePath) {
+    $isThemosis = defined('APP_TD');
+    $viewsDir = $isThemosis ? get_stylesheet_directory() . '/views' : WP_POWER_VIEWS_DIR;
     $re = '/woocommerce\/(templates\/)?(.*)/m';
 
     preg_match($re, $template, $matches);
 
-    if (isset($matches[2]) && !empty($matches[2]) && file_exists( WP_POWER_VIEWS_DIR . '/woocommerce/' . $matches[2] )) {
-        $template = WP_POWER_VIEWS_DIR . '/woocommerce/' . $matches[2];
+    if (isset($matches[2]) && !empty($matches[2]) && file_exists($viewsDir . '/woocommerce/' . $matches[2])) {
+        $template = $viewsDir . '/woocommerce/' . $matches[2];
     }
 
     return $template;
