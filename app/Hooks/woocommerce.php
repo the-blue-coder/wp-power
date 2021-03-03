9c3372d4aee1887a531c148334d9ebe69b9eb03a
Jean R <jd.rakotoarison@gmail.com>
2021-03-03 11:26:10 +0300

    update hooks/woocommerce

--- app/Hooks/woocommerce.php
+++ app/Hooks/woocommerce.php
@@ -8,7 +8,7 @@ add_filter('woocommerce_is_rest_api_request', function ($isRestAPIRequest) {
         return $isRestAPIRequest;
     }
 
-    if (strpos($_SERVER['REQUEST_URI'], WP_POWER_APP_SLUG) === false) {
+    if (defined('WP_POWER_APP_SLUG') && strpos($_SERVER['REQUEST_URI'], WP_POWER_APP_SLUG) === false) {
         return $isRestAPIRequest;
     }
 
