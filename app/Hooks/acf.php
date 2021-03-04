74ecdbba33c364885fc4793dd19ad71144010cfe
Jean R <jd.rakotoarison@gmail.com>
2021-03-04 17:10:05 +0300

    acf options page example

---
+++ app/Hooks/acf.php
@@ -0,0 +1,14 @@
+<?php
+
+/**
+ * Options page
+ */
+// add_action('init', function () {
+//     acf_add_options_page([
+//         'page_title' => 'Site options',
+//         'menu_title' => 'Site options',
+//         'menu_slug' => 'site-options',
+//         'capability' => 'edit_posts',
+//         'redirect' => false
+//     ]);
+// });
