43c075bb8aea70a0b1f18aa6d4fcd3f61d54bb75
Jean R <jd.rakotoarison@gmail.com>
2021-03-04 15:53:22 +0300

    hooks/acf

--- functions.php
+++ functions.php
@@ -35,6 +35,7 @@ require 'app/Hooks/shortcodes.php';
 require 'app/Hooks/meta-boxes.php';
 require 'app/Hooks/user-roles.php';
 require 'app/Hooks/wp-crons.php';
+require 'app/Hooks/acf.php';
 require 'app/Hooks/woocommerce.php';
 require 'app/Hooks/misc.php';
 
