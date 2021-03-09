a54c32a1520c6fb5d2943a26d3cf65911546767f
Jean R <jd.rakotoarison@gmail.com>
2021-03-09 17:40:05 +0300

    update post-types

--- app/Hooks/post-types.php
+++ app/Hooks/post-types.php
@@ -3,42 +3,46 @@
 /**
  * Post type name
  */
-// add_action('init', function () {
-//     $labels = [
-//         'name' => _x('Plural', WP_POWER_TEXT_DOMAIN),
-//         'singular_name' => _x('Singular', WP_POWER_TEXT_DOMAIN),
-//         'add_new' => __('Add new', WP_POWER_TEXT_DOMAIN),
-//         'add_new_item' => __('Add new singular', WP_POWER_TEXT_DOMAIN),
-//         'edit' => __('Edit', WP_POWER_TEXT_DOMAIN),
-//         'edit_item' => __('Edit singular', WP_POWER_TEXT_DOMAIN),
-//         'new_item' => __('New singular', WP_POWER_TEXT_DOMAIN),
-//         'view' => __('View singular', WP_POWER_TEXT_DOMAIN),
-//         'view_item' => __('View singular', WP_POWER_TEXT_DOMAIN),
-//         'search_items' => __('Search plural', WP_POWER_TEXT_DOMAIN),
-//         'not_found' => __('No plural found', WP_POWER_TEXT_DOMAIN),
-//         'not_found_in_trash' => __('No plural found in trash', WP_POWER_TEXT_DOMAIN)
-//     ];
+add_action('init', function () {
+    $slug = 'slug';
+    $dashicon = 'dashicons-admin-post';
+    $menuPosition = 20;
 
-//     $supports = [
-//         'title',
-//         'editor',
-//         'custom-fields'
-//     ];
+    $labels = [
+        'name' => _x('Plural', WP_POWER_TEXT_DOMAIN),
+        'singular_name' => _x('Singular', WP_POWER_TEXT_DOMAIN),
+        'add_new' => __('Add new', WP_POWER_TEXT_DOMAIN),
+        'add_new_item' => __('Add new singular', WP_POWER_TEXT_DOMAIN),
+        'edit' => __('Edit', WP_POWER_TEXT_DOMAIN),
+        'edit_item' => __('Edit singular', WP_POWER_TEXT_DOMAIN),
+        'new_item' => __('New singular', WP_POWER_TEXT_DOMAIN),
+        'view' => __('View singular', WP_POWER_TEXT_DOMAIN),
+        'view_item' => __('View singular', WP_POWER_TEXT_DOMAIN),
+        'search_items' => __('Search plural', WP_POWER_TEXT_DOMAIN),
+        'not_found' => __('No plural found', WP_POWER_TEXT_DOMAIN),
+        'not_found_in_trash' => __('No plural found in trash', WP_POWER_TEXT_DOMAIN)
+    ];
 
-//     register_post_type(
-//         'slug', 
-//         [
-//             'labels' => $labels,
-//             'public' => true,
-//             'publicly_queryable' => true,
-//             'show_ui' => true,
-//             'has_archive' => true,
-//             'menu_position' => 20,
-//             'menu_icon' => 'dashicons-admin-post',
-//             'show_in_menu' => true,
-//             'exclude_from_search' => false,
-//             'rewrite' => true,
-//             'supports' => $supports
-//         ]
-//     );
-// });
+    $supports = [
+        'title',
+        'editor',
+        'custom-fields'
+    ];
+
+    register_post_type(
+        $slug, 
+        [
+            'labels' => $labels,
+            'public' => true,
+            'publicly_queryable' => true,
+            'show_ui' => true,
+            'has_archive' => true,
+            'menu_position' => $menuPosition,
+            'menu_icon' => $dashicon,
+            'show_in_menu' => true,
+            'exclude_from_search' => false,
+            'rewrite' => true,
+            'supports' => $supports
+        ]
+    );
+});
