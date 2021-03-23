d4ea38c3d027523d764cec5970f81f6e3ae74cf6
Jean R <jd.rakotoarison@gmail.com>
2021-03-23 16:07:19 +0300

    misc.php hook

---
+++ app/Hooks/misc.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Filter posts by custom taxonomy in admin
+ */
+//  add_action('init', function () {
+//     $cases = [
+//         [
+//             'postType' => 'post-type-slug',
+//             'taxonomies' => ['taxonomy1', 'taxonomy2', 'taxonomy...']
+//         ],
+        
+//     ];
+
+//     WPAdminCustomTaxonomyFilters($cases, 'Filtrer par', WP_POWER_TEXT_DOMAIN);
+// });
