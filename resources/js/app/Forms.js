fa0a24ee62641294a32bafe7d9e3b2e245a9d53f
Jean R <jd.rakotoarison@gmail.com>
2021-04-15 14:38:03 +0300

    update form.js

--- resources/js/app/Forms.js
+++ resources/js/app/Forms.js
@@ -4,9 +4,9 @@ class Forms {
     constructor() {
         this.ajax = new Ajax();
 
-        this.forms = $(`
-            form.form-class
-        `);
+        this.forms = {
+            form1: $('.form1-class')
+        };
     }
 
     init() {
@@ -17,68 +17,74 @@ class Forms {
 
     ajaxSubmit() {
         let self = this;
-        
-        self.forms.on('submit', function (e) {
-            e.preventDefault();
 
-            let thisForm = $(this);
-            let thisFormID = thisForm.attr('id');
-            let method = thisForm.attr('method');
-            let endpoint = thisForm.attr('action');
-            let recaptchaAction = thisForm.attr('data-recaptcha-action');
-            let submitBtn = thisForm.find('[type="submit"]');
-            let formMessageWrapper = thisForm.find('.form-message');
-            let data = thisForm.serializeArray();
-
-            const done = function () {
-                thisForm.trigger('reset');
-            };
-
-            const fail = function (response) {
-                if (response.responseJSON.errorType === 'recaptcha') {
-                    alert('Erreur au niveau de Google Recaptcha, la page va se recharger.');
-                    window.location.reload();
-                }
-            };
-
-            const always = function (response) {
-                let message = response.message ?? response.responseJSON.message;
-
-                submitBtn.removeAttr('disabled');
-
-                if (typeof(message) !== 'undefined') {
-                    formMessageWrapper.html(message);
-                }
-            }
-
-            if (submitBtn[0].hasAttribute('disabled')) {
-                return;
-            }
-
-            if (formMessageWrapper.length === 0) {
-                formMessageWrapper = $('.form-message[data-form="#' + thisFormID + '"]');
-            }
-
-            submitBtn.attr('disabled', 'disabled');
-            formMessageWrapper.empty();
-
-            if (!recaptchaAction) {
-                self.ajax.send(endpoint, method, 'JSON', data, done, fail, always);
-                return;
-            }
-
-            grecaptcha.ready(function () {
-                grecaptcha.execute(emfps.PHPToJS.apiKeys.google.recaptcha.siteKey, {action: recaptchaAction})
-                          .then(function (recaptchaToken) {
-                              data.push({
-                                  name: 'recaptcha_token',
-                                  value: recaptchaToken
-                              });
-
-                              self.ajax.send(endpoint, method, 'JSON', data, done, fail, always);
-                          })
-                ;
+        for (let [key, form] of Object.entries(self.forms)) {
+            form.on('submit', function (e) {
+                e.preventDefault();
+                self.doAjaxSubmit(form);
             });
+        }
+    }
+
+    doAjaxSubmit(thisForm) {
+        let self = this;
+
+        let thisFormID = thisForm.attr('id');
+        let method = thisForm.attr('method');
+        let endpoint = thisForm.attr('action');
+        let recaptchaAction = thisForm.attr('data-recaptcha-action');
+        let submitBtn = thisForm.find('[type="submit"]');
+        let formMessageWrapper = thisForm.find('.form-message');
+        let data = thisForm.serializeArray();
+
+        const done = function () {
+            thisForm.trigger('reset');
+        };
+
+        const fail = function (response) {
+            if (response.responseJSON.errorType === 'recaptcha') {
+                alert('Erreur au niveau de Google Recaptcha, la page va se recharger.');
+                window.location.reload();
+            }
+        };
+
+        const always = function (response) {
+            let message = response.message ?? response.responseJSON.message;
+
+            submitBtn.removeAttr('disabled');
+
+            if (typeof(message) !== 'undefined') {
+                formMessageWrapper.html(message);
+            }
+        }
+
+        if (submitBtn[0].hasAttribute('disabled')) {
+            return;
+        }
+
+        if (formMessageWrapper.length === 0) {
+            formMessageWrapper = $('.form-message[data-form="#' + thisFormID + '"]');
+        }
+
+        submitBtn.attr('disabled', 'disabled');
+        formMessageWrapper.empty();
+
+        if (!recaptchaAction) {
+            self.ajax.send(endpoint, method, 'JSON', data, done, fail, always);
+            return;
+        }
+
+        grecaptcha.ready(function () {
+            grecaptcha.execute(emfps.PHPToJS.apiKeys.google.recaptcha.siteKey, {action: recaptchaAction})
+                      .then(function (recaptchaToken) {
+                          data.push({
+                              name: 'recaptcha_token',
+                              value: recaptchaToken
+                          });
+
+                          self.ajax.send(endpoint, method, 'JSON', data, done, fail, always);
+                      })
+            ;
         });
     }
 }
