1dfcb414923e9bfea5fce83d764266b9b2f27448
Jean R <jd.rakotoarison@gmail.com>
2021-04-24 17:41:31 +0300

    acf-options-pages

--- functions.php
+++ functions.php
@@ -36,7 +36,7 @@ require 'app/Hooks/shortcodes.php';
 require 'app/Hooks/meta-boxes.php';
 require 'app/Hooks/user-roles.php';
 require 'app/Hooks/wp-crons.php';
-require 'app/Hooks/acf.php';
+require 'app/Hooks/acf-options-pages.php';
 require 'app/Hooks/woocommerce.php';
 require 'app/Hooks/misc.php';
 
