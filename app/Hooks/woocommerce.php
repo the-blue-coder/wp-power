bdd52ea4845db29203d8cfce9a12230f9a8288ca
Jean R <jd.rakotoarison@gmail.com>
2021-06-10 10:37:57 +0300

    templates overriding update

--- app/Hooks/woocommerce.php
+++ app/Hooks/woocommerce.php
@@ -27,16 +27,6 @@ add_action('after_setup_theme', function () {
 /**
  * Templates overriding
  */
-add_filter('woocommerce_locate_template', function ($template, $templateName, $templatePath) {
-    $isThemosis = defined('APP_TD');
-    $viewsDir = $isThemosis ? get_stylesheet_directory() . '/views' : WP_POWER_VIEWS_DIR;
-    $re = '/woocommerce\/(templates\/)?(.*)/m';
-
-    preg_match($re, $template, $matches);
-
-    if (isset($matches[2]) && !empty($matches[2]) && file_exists($viewsDir . '/woocommerce/' . $matches[2])) {
-        $template = $viewsDir . '/woocommerce/' . $matches[2];
-    }
-
-    return $template;
-}, 10, 3);
+add_filter('woocommerce_template_path', function(){
+	return 'views/e-shop/woo-templates/';
+});
